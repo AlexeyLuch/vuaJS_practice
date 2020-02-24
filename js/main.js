4338429362f6fd88cbca0379016e0b5989854b27
AlexeyLuch <pochtamoyapochta123@gmail.com>
2020-02-24 11:28:45 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+Vue.filter ('capatilize', function (value) {
+    if (!value) return ''
+    value = value.toString()
+    return value.replace(/\b\w/g, function(l){ return l.toUpperCase() })
+});
+
+
+new Vue ({
+    el: "#app",
+    data: {
+       show:false,
+       message: 'hello worl, hello',
+       cars: [
+          {model: "rew", speed: 250.8},
+          {model: "Audi",speed: 240.21},
+          {model: "Mersedes-Benz AMG", speed: 350.4},
+          {model: "Ford", speed: 160.5}
+       ]
+
+    },
+    computed: {
+        showMess () {
+            return this.message.toUpperCase()
+       }
+    },
+
+    filters: {
+        lowercase (value) {
+            return value.toLowerCase()
+        }
+    }
+});
